2898a1a52f2647dabc52240a1499cd8490ae6019
vrunoa <vruno.alassia@gmail.com>
2015-07-20 16:42:37 -0300

    update scripts

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,10 @@ describe("is-port-taken tests", () => {
 
   it("should return boolean", (done) => {
     let flag = isTaken(8009, '127.0.0.1');
-  })
+    console.log(flag);
+    if(flag === true || flag === false) done();
+    throw new Error("returns incorrect value: "+flag);
+  });
 
 });
  
