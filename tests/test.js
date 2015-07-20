1383d14b5a240482100aaff6a897c59e5dea0f8b
vrunoa <vruno.alassia@gmail.com>
2015-07-20 18:15:51 -0300

    add test, use promises

--- tests/test.js
+++ tests/test.js
@@ -1,13 +1,28 @@
 import isTaken from '../lib/';
+import net from 'net';
 
 describe("is-port-taken tests", () => {
 
   it("should return boolean", (done) => {
-    let flag = isTaken(8009, '127.0.0.1');
-    console.log(flag);
-    if(flag === true || flag === false) done();
-    throw new Error("returns incorrect value: "+flag);
+    let promise = isTaken(8009, '127.0.0.1');
+    promise.then( flag => {
+      if(flag === true || flag === false) done();
+      throw new Error("returns incorrect value: "+flag);
+    });
   });
+
+  it("should return true", (done) => {
+    // create temp server
+    let port = 8009, host = '127.0.0.1';
+    var server = net.createServer(null, () => {});
+    server.listen(port, host, (err) => {});
+    let promise = isTaken(port, host);
+    promise.then( (flag) => {
+      server.close();
+      if(flag == true) done();
+      throw new Error("Error checking on temp server");
+    });
+  })
 
 });
  
